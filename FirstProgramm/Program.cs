9e5455ea661b22a1f8d54253cf6edb5d08a61a57
Анна Башкатова <annabashk13@gmail.com>
2022-08-21 11:42:22 +0300

    Добавьте файлы проекта.

---
+++ FirstProgramm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstProgramm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Anna";
+            int Age = 23;
+            bool Pet = true;
+            double Shoe = 39.5;
+
+            Console.WriteLine("My mame is " + Name);
+            Console.WriteLine("My age " + Age);
+            Console.WriteLine("Do I have a pet? " + Pet);
+            Console.WriteLine("My shoe size is " + Shoe);
+        }
+    }
+}
